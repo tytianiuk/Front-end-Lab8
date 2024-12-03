dfb7b70afdd9b878aff696c08b91c02bec6063d4
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-03 23:00:20 +0200

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        comfortaa: ['Comfortaa', 'cursive'],
+        merriweather: ['Merriweather', 'serif'],
+        mvboli: ['MV Boli', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
